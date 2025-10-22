1c57af316533880a236cffa82a49dd7c0fb7731c
naxope <nachoapereyra@gmail.com>
2025-10-22 10:31:49 -0300

    Agrego cositas

--- src/Screens/landingPage/index.tsx
+++ src/Screens/landingPage/index.tsx
@@ -4,7 +4,7 @@ import AppShell from '../../components/Shell';
 export default function LandingPage() {
   return (
     <AppShell>
-      <div className={styles.landingContent}>
+      <div className={styles.landingContent}> 
         <h1 className={styles.landingTitle}>TITULO</h1>
       </div>
     </AppShell>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 export default function Header() {
   return (
     <header className={styles.appHeader}>
-      <h1 className={styles.title}>FIUBA Groups Frontend</h1>
+      <h1 className={styles.title}>FIUBA Groups</h1>
     </header>
   );
 }
