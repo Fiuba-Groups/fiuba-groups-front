47244f6105e1866a5a29d5e5f31c1f6c402a427e
naxope <nachoapereyra@gmail.com>
2025-10-22 14:26:51 -0300

    Login page  working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import LandingPage from './Screens/landingPage/index';
-import { Routes, Route } from 'react-router-dom';
+import LoginScreen from './Screens/LoginScreen/LoginScreen';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route 
+        path="/" 
+        element={<Navigate to="/login" replace />} 
+      />
+      <Route 
+        path="/login" 
+        element={
+          !isLoggedIn ? (
+            <LoginScreen onLogin={() => setIsLoggedIn(true)} />
+          ) : (
+            <Navigate to="/home" replace />
+          )
+        } 
+      />
+      <Route 
+        path="/home" 
+        element={
+          isLoggedIn ? (
+            <LandingPage />
+          ) : (
+            <Navigate to="/login" replace />
+          )
+        } 
+      />
     </Routes>
   );
 }
